629b78c3184155bd6c872f5c387f3fcba3aa7262
lee nam kyeong <skarod0401@naver.com>
2021-04-04 21:27:13 +0900

    2021.04.04이남경.junit 설정 완료
    1. junit 설정 완료
    2. 백로그 css 경로 수정
    3. 백로그 경로 설정

---
+++ src/main/java/com/greedy/shortcut/board/controller/BacklogBoardController.java
@@ -0,0 +1,13 @@
+package com.greedy.shortcut.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board/*")
+public class BacklogBoardController {
+	
+	@GetMapping("backlog")
+	public void project() {}
+}
