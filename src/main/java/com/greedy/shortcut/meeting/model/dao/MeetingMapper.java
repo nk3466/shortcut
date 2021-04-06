9f5d7f69ec7205d379c36ca9be1461e614b34016
DESKTOP-TQ5I0BH\WANGYU <kordzor@gmail.com>
2021-04-06 19:09:54 +0900

    미팅로그 insert작업
    부모키 오류발생 아직 값이 없어서 오류남

---
+++ src/main/java/com/greedy/shortcut/meeting/model/dao/MeetingMapper.java
@@ -0,0 +1,9 @@
+package com.greedy.shortcut.meeting.model.dao;
+
+import com.greedy.shortcut.meeting.model.dto.MeetingDTO;
+
+public interface MeetingMapper {
+
+	int insertMeeting(MeetingDTO meeting);
+
+}
---
+++ src/main/java/com/greedy/shortcut/meeting/model/service/MeetingService.java
@@ -0,0 +1,9 @@
+package com.greedy.shortcut.meeting.model.service;
+
+import com.greedy.shortcut.meeting.model.dto.MeetingDTO;
+
+public interface MeetingService {
+
+	boolean insertMeeting(MeetingDTO meeting);
+
+}
