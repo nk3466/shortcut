0b2e538ce6c7c0be6ed38626209ac51edea35fcb
DESKTOP-GJN1LJM\HMH <hmh86116@gmail.com>
2021-04-07 10:17:45 +0900

    카드 DTO 생성
    카드 DTO 생성함

---
+++ src/main/java/com/greedy/shortcut/board/model/dto/CardScheduleDTO.java
@@ -0,0 +1,82 @@
+package com.greedy.shortcut.board.model.dto;
+
+import java.sql.Date;
+import java.util.List;
+
+public class CardScheduleDTO {
+
+	private int alert;                  //일정알림
+	private String place;               //일정장소
+	private int crdNo;                  // 카드번호
+	private java.sql.Date startDate;    //일정 시작일
+	private java.sql.Date endDate;      //일정 종료일
+	
+	private List<CardAttendListDTO> cardAttendList;   //일정참석여부
+	
+	public CardScheduleDTO() {}
+
+	public CardScheduleDTO(int alert, String place, int crdNo, Date startDate, Date endDate,
+			List<CardAttendListDTO> cardAttendList) {
+		super();
+		this.alert = alert;
+		this.place = place;
+		this.crdNo = crdNo;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.cardAttendList = cardAttendList;
+	}
+
+	public int getAlert() {
+		return alert;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public int getCrdNo() {
+		return crdNo;
+	}
+
+	public java.sql.Date getStartDate() {
+		return startDate;
+	}
+
+	public java.sql.Date getEndDate() {
+		return endDate;
+	}
+
+	public List<CardAttendListDTO> getCardAttendList() {
+		return cardAttendList;
+	}
+
+	public void setAlert(int alert) {
+		this.alert = alert;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+
+	public void setCrdNo(int crdNo) {
+		this.crdNo = crdNo;
+	}
+
+	public void setStartDate(java.sql.Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(java.sql.Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public void setCardAttendList(List<CardAttendListDTO> cardAttendList) {
+		this.cardAttendList = cardAttendList;
+	}
+
+	@Override
+	public String toString() {
+		return "CardScheduleDTO [alert=" + alert + ", place=" + place + ", crdNo=" + crdNo + ", startDate=" + startDate
+				+ ", endDate=" + endDate + ", cardAttendList=" + cardAttendList + "]";
+	}
+}
---
+++ src/main/java/com/greedy/shortcut/board/model/dto/CardTaskDTO.java
@@ -0,0 +1,69 @@
+package com.greedy.shortcut.board.model.dto;
+
+import java.sql.Date;
+
+public class CardTaskDTO {
+
+	private int progress;                  //업무진척도
+	private int crdNo;                     //카드번호
+	private int memNo;                     //담당자
+	private java.sql.Date startDate;       //업무시작일
+	private java.sql.Date endDate;         //업무종료일
+	
+	public CardTaskDTO() {}
+
+	public CardTaskDTO(int progress, int crdNo, int memNo, Date startDate, Date endDate) {
+		super();
+		this.progress = progress;
+		this.crdNo = crdNo;
+		this.memNo = memNo;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	public int getProgress() {
+		return progress;
+	}
+
+	public int getCrdNo() {
+		return crdNo;
+	}
+
+	public int getMemNo() {
+		return memNo;
+	}
+
+	public java.sql.Date getStartDate() {
+		return startDate;
+	}
+
+	public java.sql.Date getEndDate() {
+		return endDate;
+	}
+
+	public void setProgress(int progress) {
+		this.progress = progress;
+	}
+
+	public void setCrdNo(int crdNo) {
+		this.crdNo = crdNo;
+	}
+
+	public void setMemNo(int memNo) {
+		this.memNo = memNo;
+	}
+
+	public void setStartDate(java.sql.Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public void setEndDate(java.sql.Date endDate) {
+		this.endDate = endDate;
+	}
+
+	@Override
+	public String toString() {
+		return "CardTaskDTO [progress=" + progress + ", crdNo=" + crdNo + ", memNo=" + memNo + ", startDate="
+				+ startDate + ", endDate=" + endDate + "]";
+	}
+}
