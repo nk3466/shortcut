66f9c2e62559ec43b4dd41dfcf9adbafa8dc4e58
LAPTOP-GHEVJHE5\cjhoo <cjhoon1992@gmail.com>
2021-04-07 18:24:01 +0900

    1. mybatis-config 수정 (CARD, 연관 DTO 추가 ) 2. css 수정 3. mywork jsp 수정 4. MyworkController,Mapper, Card And Task DTO, Service, ServiceImpl, Mapper 추가 5. pagenation.js 추가 ( 추후 공통으로 변경할 예정임) 6. mywork-0407 ( 이전 mywork 백업 )

---
+++ src/main/java/com/greedy/shortcut/mywork/model/service/MyworkService.java
@@ -0,0 +1,18 @@
+package com.greedy.shortcut.mywork.model.service;
+
+import java.util.List;
+
+import com.greedy.shortcut.member.model.dto.MemberDTO;
+import com.greedy.shortcut.mywork.model.dto.ClientCardDTO;
+import com.greedy.shortcut.mywork.model.dto.ClientTaskDTO;
+import com.greedy.shortcut.mywork.model.dto.MyworkResponseCardAndTaskDTO;
+
+public interface MyworkService {
+
+	MemberDTO selectUserInfoOne(String userName);
+
+	List<ClientCardDTO> selectCardList(Integer no);
+
+	List<MyworkResponseCardAndTaskDTO> selectTaskList(Integer no);
+
+}
