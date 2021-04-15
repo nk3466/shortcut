0bfa36d57aec6fb4fd29c2e024ec9be234cfc8f6
minseo23 <neverdie4757@gmail.com>
2021-04-16 00:37:06 +0900

    조회 일부 수정

--- src/main/java/com/greedy/shortcut/board/model/dto/RequestCardDTO.java
+++ src/main/java/com/greedy/shortcut/board/model/dto/RequestCardDTO.java
@@ -3,8 +3,6 @@ package com.greedy.shortcut.board.model.dto;
 import java.sql.Date;
 import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class RequestCardDTO {
 	
 	private int no;
@@ -16,7 +14,7 @@ public class RequestCardDTO {
 	private int type;
 	private int memNo;
 	private String txt;
-	private List<Integer> memberList;
+	private List<CardAttendListDTO> memberList;
 	private int member; //담당자
 	private String place;
 	private int alert;
@@ -29,7 +27,7 @@ public class RequestCardDTO {
 	public RequestCardDTO() {}
 
 	public RequestCardDTO(int no, String title, Date enrollDate, String delNo, int brdNo, int order, int type,
-			int memNo, String txt, List<Integer> memberList, int member, String place, int alert,
+			int memNo, String txt, List<CardAttendListDTO> memberList, int member, String place, int alert,
 			String scheduleStartDate, String scheduleEndDate, Date taskStartDate, Date taskEndDate) {
 		super();
 		this.no = no;
@@ -55,132 +53,132 @@ public class RequestCardDTO {
 		return no;
 	}
 
-	public String getTitle() {
-		return title;
-	}
-
-	public java.sql.Date getEnrollDate() {
-		return enrollDate;
-	}
-
-	public String getDelNo() {
-		return delNo;
-	}
-
-	public int getBrdNo() {
-		return brdNo;
-	}
-
-	public int getOrder() {
-		return order;
-	}
-
-	public int getType() {
-		return type;
-	}
-
-	public int getMemNo() {
-		return memNo;
-	}
-
-	public String getTxt() {
-		return txt;
-	}
-
-	public List<Integer> getMemberList() {
-		return memberList;
-	}
-
-	public int getMember() {
-		return member;
-	}
-
-	public String getPlace() {
-		return place;
-	}
-
-	public int getAlert() {
-		return alert;
-	}
-
-	public String getScheduleStartDate() {
-		return scheduleStartDate;
-	}
-
-	public String getScheduleEndDate() {
-		return scheduleEndDate;
-	}
-
-	public java.sql.Date getTaskStartDate() {
-		return taskStartDate;
-	}
-
-	public java.sql.Date getTaskEndDate() {
-		return taskEndDate;
-	}
-
 	public void setNo(int no) {
 		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
 	}
 
 	public void setTitle(String title) {
 		this.title = title;
 	}
 
+	public java.sql.Date getEnrollDate() {
+		return enrollDate;
+	}
+
 	public void setEnrollDate(java.sql.Date enrollDate) {
 		this.enrollDate = enrollDate;
+	}
+
+	public String getDelNo() {
+		return delNo;
 	}
 
 	public void setDelNo(String delNo) {
 		this.delNo = delNo;
 	}
 
+	public int getBrdNo() {
+		return brdNo;
+	}
+
 	public void setBrdNo(int brdNo) {
 		this.brdNo = brdNo;
+	}
+
+	public int getOrder() {
+		return order;
 	}
 
 	public void setOrder(int order) {
 		this.order = order;
 	}
 
+	public int getType() {
+		return type;
+	}
+
 	public void setType(int type) {
 		this.type = type;
+	}
+
+	public int getMemNo() {
+		return memNo;
 	}
 
 	public void setMemNo(int memNo) {
 		this.memNo = memNo;
 	}
 
+	public String getTxt() {
+		return txt;
+	}
+
 	public void setTxt(String txt) {
 		this.txt = txt;
 	}
 
-	public void setMemberList(List<Integer> memberList) {
+	public List<CardAttendListDTO> getMemberList() {
+		return memberList;
+	}
+
+	public void setMemberList(List<CardAttendListDTO> memberList) {
 		this.memberList = memberList;
+	}
+
+	public int getMember() {
+		return member;
 	}
 
 	public void setMember(int member) {
 		this.member = member;
 	}
 
+	public String getPlace() {
+		return place;
+	}
+
 	public void setPlace(String place) {
 		this.place = place;
+	}
+
+	public int getAlert() {
+		return alert;
 	}
 
 	public void setAlert(int alert) {
 		this.alert = alert;
 	}
 
+	public String getScheduleStartDate() {
+		return scheduleStartDate;
+	}
+
 	public void setScheduleStartDate(String scheduleStartDate) {
 		this.scheduleStartDate = scheduleStartDate;
+	}
+
+	public String getScheduleEndDate() {
+		return scheduleEndDate;
 	}
 
 	public void setScheduleEndDate(String scheduleEndDate) {
 		this.scheduleEndDate = scheduleEndDate;
 	}
 
+	public java.sql.Date getTaskStartDate() {
+		return taskStartDate;
+	}
+
 	public void setTaskStartDate(java.sql.Date taskStartDate) {
 		this.taskStartDate = taskStartDate;
+	}
+
+	public java.sql.Date getTaskEndDate() {
+		return taskEndDate;
 	}
 
 	public void setTaskEndDate(java.sql.Date taskEndDate) {
@@ -198,4 +196,4 @@ public class RequestCardDTO {
 	
 	
 	
-}
+}	
