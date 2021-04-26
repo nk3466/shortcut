3cff28214308d1afa033b0b706a974fd3e36e88f
DESKTOP-TQ5I0BH\WANGYU <kordzor@gmail.com>
2021-04-26 23:55:16 +0900

    JUnit  insert test 일부 완료

---
+++ src/test/java/com/greedy/shortcut/meeting/controller/MeetingLogControllerTest2.java
@@ -0,0 +1,63 @@
+package com.greedy.shortcut.meeting.controller;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import com.greedy.shortcut.meeting.model.dao.MeetingMapper;
+import com.greedy.shortcut.meeting.model.dto.MeetingDTO;
+
+public class MeetingLogControllerTest2 {
+
+	@Mock
+	MeetingMapper meetingMapper;
+	
+	@Test
+	public void testMeetingLogController() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMeetinglog() {
+		MeetingDTO meetingList =  new MeetingDTO();
+		
+	}
+
+	@Test
+	public void testMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectProjectMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMeetingList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectMeetingDetail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletedMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifyMeeting() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testModifyComplete() {
+		fail("Not yet implemented");
+	}
+
+}
